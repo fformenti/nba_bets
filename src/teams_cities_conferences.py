7665e64bb3f09f71442839b1a6c286ffd318ee39
felipe.ferreira <fferreira@ettworld.com>
2025-04-08 18:50:48 -0300

    teams_cities table creation improvement

--- src/teams_cities_conferences.py
+++ src/teams_cities_conferences.py
@@ -1,27 +1,22 @@
 import pandas as pd
+from datetime import datetime
+
 
 EAST_CITIES = [
-    "St. Louis",
     "Orlando",
     "New York",
     "Miami",
     "Detroit",
-    "Buffalo",
     "Washington",
     "Milwaukee",
-    "Toronto",
     "Chicago",
-    "Charlotte",
     "Philadelphia",
-    "Cincinnati",
-    "Baltimore",
     "Cleveland",
-    "New Jersey",
     "Boston",
-    "Capital",
     "Atlanta",
     "Indiana",
-    "Brooklyn",
+    "Charlotte",
+    "New Jersey",
 ]
 
 WEST_CITIES = [
@@ -29,33 +24,145 @@ WEST_CITIES = [
     "Phoenix",
     "San Antonio",
     "Golden State",
-    "Seattle",
     "Portland",
     "Denver",
     "Utah",
-    "Oklahoma City",
+    "Seatle",
     "Sacramento",
-    "San Francisco",
-    "Vancouver",
     "Dallas",
-    "Kansas City",
-    "Kansas City-Omaha",
     "Houston",
-    "Memphis",
-    "New Orleans",
     "Minnesota",
-    "San Diego",
 ]
 
 
 def get_cities_conference():
-    east_cities_df = pd.MultiIndex.from_product(
-        [EAST_CITIES, ["East"]], names=["teamCity", "Conference"]
+    # === EAST CITIES ===
+
+    # Toronto Added
+    start_date = "1995-07-01"
+    end_date = "2001-07-01"
+    EAST_CITIES.append("Toronto")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    east_cities_1995_2001 = pd.MultiIndex.from_product(
+        [EAST_CITIES, date_range, ["East"]],
+        names=["teamCity", "date", "Conference"],
     ).to_frame(index=False)
 
-    west_cities_df = pd.MultiIndex.from_product(
-        [WEST_CITIES, ["West"]], names=["teamCity", "Conference"]
+    # Charlotte --> New Orleans
+    start_date = "2002-07-01"
+    end_date = "2003-07-01"
+    EAST_CITIES.remove("Charlotte")
+    EAST_CITIES.append("New Orleans")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    east_cities_2002_2003 = pd.MultiIndex.from_product(
+        [EAST_CITIES, date_range, ["East"]],
+        names=["teamCity", "date", "Conference"],
     ).to_frame(index=False)
 
-    cities_conferences = pd.concat([east_cities_df, west_cities_df], axis=0)
+    # Charlotte Re-Added and New Orleans changed Conference
+    start_date = "2004-07-01"
+    end_date = "2011-07-01"
+    EAST_CITIES.remove("New Orleans")
+    EAST_CITIES.append("Charlotte")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    east_cities_2004_2011 = pd.MultiIndex.from_product(
+        [EAST_CITIES, date_range, ["East"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # New Jersey --> Broolyn
+    start_date = "2012-07-01"
+    end_date = datetime.today().strftime("%Y-%m-%d")
+    EAST_CITIES.remove("New Jersey")
+    EAST_CITIES.append("Brooklyn")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    east_cities_2012_2100 = pd.MultiIndex.from_product(
+        [EAST_CITIES, date_range, ["East"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # === WEST CITIES ===
+    # Vancouver Added
+    start_date = "1995-07-01"
+    end_date = "2000-07-01"
+    WEST_CITIES.append("Vancouver")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_1995_2000 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # Vancouver --> Memphis
+    start_date = "2001-07-01"
+    end_date = "2003-07-01"
+    WEST_CITIES.remove("Vancouver")
+    WEST_CITIES.append("Memphis")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_2001_2003 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # Charlote --> New Orleans
+    start_date = "2004-07-01"
+    end_date = "2004-07-01"
+    WEST_CITIES.append("New Orleans")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_2004_2004 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # New Orleans with Oklahoma City -- Katrina
+    start_date = "2005-07-01"
+    end_date = "2006-07-01"
+    WEST_CITIES.append("Oklahoma City")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_2005_2006 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # New Orleans full time
+    start_date = "2008-07-01"
+    end_date = "2008-07-01"
+    WEST_CITIES.remove("Oklahoma City")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_2007_2007 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # Seatle --> Oklahoma City
+    start_date = "2009-07-01"
+    WEST_CITIES.remove("Seatle")
+    WEST_CITIES.append("Oklahoma City")
+    end_date = datetime.today().strftime("%Y-%m-%d")
+    date_range = pd.date_range(start=start_date, end=end_date, freq="YE")
+    west_cities_2008_2100 = pd.MultiIndex.from_product(
+        [WEST_CITIES, date_range, ["West"]],
+        names=["teamCity", "date", "Conference"],
+    ).to_frame(index=False)
+
+    # === Concat ===
+    cities_conferences = pd.concat(
+        [
+            east_cities_1995_2001,
+            east_cities_2002_2003,
+            east_cities_2004_2011,
+            east_cities_2012_2100,
+            west_cities_1995_2000,
+            west_cities_2001_2003,
+            west_cities_2004_2004,
+            west_cities_2005_2006,
+            west_cities_2007_2007,
+            west_cities_2008_2100,
+        ],
+        axis=0,
+    )
     return cities_conferences
+
+
+if __name__ == "__main__":
+    cities_conferences = get_cities_conference()
+    print(cities_conferences)
